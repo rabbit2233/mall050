52705490b7b4d527cdf44381222914a50abc854e
wangzhe <safeboru@163.com>
2020-07-06 20:58:23 +0800

    加入用户管理模块

---
+++ mall050-user-manage/src/main/java/com/mall/mall050/user/Mall050UserManageApplication.java
@@ -0,0 +1,13 @@
+package com.mall.mall050.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mall050UserManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mall050UserManageApplication.class, args);
+    }
+
+}
